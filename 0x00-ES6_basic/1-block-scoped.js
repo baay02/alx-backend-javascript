e018db4a0828fa80afa4225668251e83e79bc619
baay02 <baaybilya25@yahoo.com>
2023-06-26 22:43:31 +0000

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
-/* eslint-disable no-unusable-var */
-/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
+  /* eslint-enable no-unused-vars */
 
   return [task, task2];
 }
